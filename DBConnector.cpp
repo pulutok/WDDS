4ed1120569783bf5af5393ca28fcd324717952b9
Jeon SangHyeon <skyclad0x7b7@gmail.com>
2017-02-02 00:12:55 -0800

    Design DB Connector (re-commit)

--- DBConnector.cpp
+++ DBConnector.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "DBConnector.h"
+
+// Initializer
+DBConnector::DBConnector() :
+    m_initialized(false)
+{
+
+}
+
+// Destructor
+DBConnector::~DBConnector()
+{
+
+}
+
+void DBConnector::disconnection()
+{
+
+}
+
+bool DBConnector::initialize(std::string db_server_ip, std::string db_user, std::string db_pwd, std::string db_name,
+                             int port)
+{
+
+}
+
+bool DBConnector::is_initialized()
+{
+    return m_initialized;
+}
--- DBConnector.h
+++ DBConnector.h
@@ -1,13 +1,27 @@
 //
 // Created by skyclad on 1/31/17.
 //
+#pragma once
 
 #ifndef WDDS_DBCONNECTOR_H
 #define WDDS_DBCONNECTOR_H
 
+#include <mysql/mysql.h>
+#include <string>
 
 class DBConnector {
+private:
+    MYSQL *m_connection;
+    bool m_initialized;
 
+public:
+    DBConnector();
+    ~DBConnector();
+
+    bool initialize(std::string db_server_ip, std::string db_user, std::string db_pwd, std::string db_name, int port);
+    void disconnection();
+
+    bool is_initialized();
 };
 
 
